434963c899714b511c44d3c1046c9aae6ee75559
Lwmay <45488236+Lwmay@users.noreply.github.com>
2020-07-17 16:04:54 +0400

    Initial commit

---
+++ scr/Container.php
@@ -0,0 +1,26 @@
+<?php
+
+
+namespace lwmay\DependencyInjection;
+
+use Psr\Container\ContainerExceptionInterface;
+use Psr\Container\ContainerInterface;
+use Psr\Container\NotFoundExceptionInterface;
+
+/**
+ * Class Container
+ * @package lwmay\DependencyInjection
+ */
+class Container implements ContainerInterface
+{
+
+    public function get($id)
+    {
+        // TODO: Implement get() method.
+    }
+
+    public function has($id)
+    {
+        // TODO: Implement has() method.
+    }
+}
---
+++ test/ContainerTest.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace lwmay\DependencyInjection\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class ContainerTest
+ * @package lwmay\DependencyInjection\Tests
+ */
+class ContainerTest extends TestCase
+{
+
+}
